1525997af1c6e6d71ad21c5e18bda126c46d82e6
Samer <samer@kindlingllc.com>
2021-11-15 16:56:40 -0600

    modify production settings

--- django_advent/settings/production.py
+++ django_advent/settings/production.py
@@ -2,10 +2,10 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['advent.herokuapp.com',]
+ALLOWED_HOSTS = ['enigmatic-stream-60815.herokuapp.com',]
 
 SECURE_SSL_REDIRECT = True
 
-STATIC_URL = 'https://{0}/{1}/'.format(
-    AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = 'https://{0}/{1}/'.format(
+#     AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
