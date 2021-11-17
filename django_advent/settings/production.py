98f8290aa07dffc659bae251187425bf719130dc
Samer <samer@kindlingllc.com>
2021-11-16 20:21:00 -0600

    add static url

--- django_advent/settings/production.py
+++ django_advent/settings/production.py
@@ -9,3 +9,5 @@ SECURE_SSL_REDIRECT = True
 # STATIC_URL = 'https://{0}/{1}/'.format(
 #     AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
